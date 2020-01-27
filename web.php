b5fb081ff420c3f25cc8bf0111caadcddea804f7
Yosuke Suzuki <pvc100kie@gmail.com>
2020-01-27 11:12:44 +0000

    0127task

--- web.php
+++ web.php
@@ -22,12 +22,19 @@ Route::get('admin/news/create', function () {
 });
 */
 
-Route::prefix('admin/profile')->group(function() {
-   Route::get('create', 'Admin\ProfileController@add')->middleware('auth'); 
-   Route::get('edit', 'Admin\ProfileController@edit') ->middleware('auth');
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
+   Route::get('profile/create', 'Admin\ProfileController@add'); 
+   Route::get('profile/edit', 'Admin\ProfileController@edit');
+   Route::post('profile/create', 'Admin\ProfileController@create');
+   Route::post('profile/edit', 'Admin\ProfileController@update');
 });
 
-Route::get('admin/news/create', 'Admin\NewsController@add')->middleware('auth');
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
+     Route::get('news/create', 'Admin\NewsController@add');
+     Route::post('news/create', 'Admin\NewsController@create'); # 追記
+});
+
+//Route::get('admin/news/create', 'Admin\NewsController@add')->middleware('auth');
 
 /*
 Route::group(['prefix' => 'admin'], function() {
